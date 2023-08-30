c3131e8fb204f8de119cd0645ec84923618bbf3e
switchII <luoandon@gmail.com>
2023-08-31 01:12:28 +0800

    feat:集成数据库配置

--- alinesno-infra-ops-scheduler-boot/src/main/java/com/alinesno/infra/ops/scheduler/OpsSchedulerApplication.java
+++ alinesno-infra-ops-scheduler-boot/src/main/java/com/alinesno/infra/ops/scheduler/OpsSchedulerApplication.java
@@ -1,15 +1,16 @@
 package com.alinesno.infra.ops.scheduler;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 集成一个Java开发示例工具
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.alinesno.infra.ops.scheduler.mapper")
+@SpringBootApplication
 public class OpsSchedulerApplication {
 
 	public static void main(String[] args) {
