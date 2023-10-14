0718d8300b41a008b37525396a34adf117cba57c
switchII <luoandon@gmail.com>
2023-10-14 08:33:05 +0800

    feat:集成dag

---
+++ alinesno-infra-ops-scheduler-admin/src/views/system/user/components/ProgressNode.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    this is a test {{ percentage }}
+    <el-progress type="dashboard" :percentage="percentage" :color="colors" />
+  </div>
+</template>
+
+<script setup>
+import { inject, ref, onMounted } from 'vue';
+
+const getNode = inject('getNode');
+const percentage = ref(80);
+
+const colors = [
+  { color: '#f56c6c', percentage: 20 },
+  { color: '#e6a23c', percentage: 40 },
+  { color: '#5cb87a', percentage: 60 },
+  { color: '#1989fa', percentage: 80 },
+  { color: '#6f7ad3', percentage: 100 },
+]
+
+onMounted(() => {
+  const node = getNode();
+  console.log(node);
+});
+
+</script>
